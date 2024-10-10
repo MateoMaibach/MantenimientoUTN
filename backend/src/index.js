95e7053de84f6c56124127b9dc606ceeaa99139b
Mateo Maibach <mateo1maibach@gmail.com>
2024-10-10 19:36:09 -0300

    se agregaron variables de entorno

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,7 @@
-import express from 'express'
-import cors from 'cors'
-import auth from './routes/auth.routes.js'
-import operarios from './routes/operarios.routes.js'
-import edificio from './routes/edificio.routes.js'
-import piso from './routes/piso.routes.js'
-import sector from './routes/sector.routes.js'
-import ubicacion from './routes/ubicacion.routes.js'
-import activo from './routes/activo.routes.js'
-import usuarios from './routes/usuarios.routes.js'
-import tareas from './routes/tarea.routes.js'
-const app = express();
+import app from './app.js'
+import { PORT } from './config.js'
 
-app.use(cors({
-    origin: 'http://localhost:4200', 
-    methods: 'GET,POST,PUT,DELETE', 
-    allowedHeaders: 'Content-Type,Authorization' 
-}));
+app.listen(PORT)
+console.log('Server on port', PORT)
 
-app.get('/', (req, res) => {
-    res.json({
-        text: 'api works!'
-    })
-})
-
-app.use(auth, operarios, edificio, piso, sector, ubicacion, activo, usuarios, tareas)
-
-
-
-
-
-
-app.listen(3000, () => {
-    console.log('Server on port 3000')
-});
 
