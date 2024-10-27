4ce7bed601d210c4844679c59f1cd5cc7156b795
Mateo Maibach <mateo1maibach@gmail.com>
2024-10-26 23:54:44 -0300

    cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,38 +1,46 @@
-import express from 'express'
-import cors from 'cors'
-import {PORT} from './config.js' 
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
+import auth from './routes/auth.routes.js';
+import operarios from './routes/operarios.routes.js';
+import edificio from './routes/edificio.routes.js';
+import piso from './routes/piso.routes.js';
+import sector from './routes/sector.routes.js';
+import ubicacion from './routes/ubicacion.routes.js';
+import activo from './routes/activo.routes.js';
+import usuarios from './routes/usuarios.routes.js';
+import tareas from './routes/tarea.routes.js';
 
-import auth from './routes/auth.routes.js'
-import operarios from './routes/operarios.routes.js'
-import edificio from './routes/edificio.routes.js'
-import piso from './routes/piso.routes.js'
-import sector from './routes/sector.routes.js'
-import ubicacion from './routes/ubicacion.routes.js'
-import activo from './routes/activo.routes.js'
-import usuarios from './routes/usuarios.routes.js'
-import tareas from './routes/tarea.routes.js'
 const app = express();
 
+
 app.use(cors({
-    origin: 'http://localhost:4200', 
+    origin: 'http://localhost:4200',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
-    credentials: true 
+    credentials: true
 }));
 
 
-app.get('/', (req, res) => {
-    res.json({
-        text: 'api works!'
-    })
-})
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
-app.use(auth, operarios, edificio, piso, sector, ubicacion, activo, usuarios, tareas)
+app.get('/', (req, res) => {
+    res.json({ text: 'API works!' });
+});
+
+
+app.use(auth);
+app.use(operarios);
+app.use(edificio);
+app.use(piso);
+app.use(sector);
+app.use(ubicacion);
+app.use(activo);
+app.use(usuarios);
+app.use(tareas);
 
 export default app;
-
