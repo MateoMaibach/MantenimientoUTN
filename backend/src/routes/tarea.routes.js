dad4c1a73feaef97a6b3efe14dd130743524daf8
IvanSekul147 <137842170+IvanSekul147@users.noreply.github.com>
2024-11-10 03:40:49 -0300

    tareasrelacionadas

--- backend/src/routes/tarea.routes.js
+++ backend/src/routes/tarea.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getTareas, getTarea } from "../controllers/tarea.controller.js";
-
+import { getTareas, getTarea, getTareasPorActivoYGrupo } from '../controllers/tarea.controller.js';
 const router = Router();
 
 router.get('/api/tareas',getTareas)
 router.get('/api/tareas/:id_tarea', getTarea);
+router.get('/tareas-por-activo-grupo', getTareasPorActivoYGrupo);
 
 export default router
