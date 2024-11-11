42401a94799968cb3e9325c1de39a7d0b93884b9
IvanSekul147 <137842170+IvanSekul147@users.noreply.github.com>
2024-11-11 01:07:53 -0300

    impletacion de tareas

--- backend/src/controllers/ordentrabajo.controller.js
+++ backend/src/controllers/ordentrabajo.controller.js
@@ -1,40 +1,41 @@
 import { pool } from '../db.js';
 
-
 export const postOrden = async (req, res) => {
     try {
         const {
             OT_num,
             fecha,
             observacion,
-            edificio_nombre,  
+            edificio_nombre,
             tarea_descripcion,
-            sector_nombre,  
-            piso_nombre,    
+            sector_nombre,
+            piso_nombre,
             ubicacion_descripcion,
             operario_username,
             tipo_activo,
+            tareas // Recibimos las tareas como JSON
         } = req.body;
 
-        
         const query = `INSERT INTO ordentrabajo 
                         (OT_num, fecha, observacion, codigo_unico,
                         edificio_nombre, tarea_descripcion, sector_nombre,
-                        piso_nombre, ubicacion_descripcion, operario_username, tipo_activo) 
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+                        piso_nombre, ubicacion_descripcion, operario_username,
+                        tipo_activo, tareas) 
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
         const [rows] = await pool.query(query, [
             OT_num,
             fecha,
             observacion,
-            '', 
-            edificio_nombre,  
+            '', // código único
+            edificio_nombre,
             tarea_descripcion,
-            sector_nombre,     
-            piso_nombre,       
+            sector_nombre,
+            piso_nombre,
             ubicacion_descripcion,
             operario_username,
             tipo_activo,
+            tareas // Almacenamos JSON en la columna tareas
         ]);
 
         res.status(201).json({
@@ -45,7 +46,6 @@ export const postOrden = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
-
 
 export const getOrdenes = async (req, res) => {
     try {
@@ -58,16 +58,14 @@ export const getOrdenes = async (req, res) => {
 
 export const deleteOrden = async (req, res) => {
     try {
-        const { id } = req.params; 
+        const { id } = req.params;
 
         const [orderRows] = await pool.query('SELECT * FROM ordentrabajo WHERE id_orden = ?', [id]);
         if (orderRows.length === 0) {
             return res.status(404).json({ message: 'Orden de trabajo no encontrada' });
         }
 
-        
         const [result] = await pool.query('DELETE FROM ordentrabajo WHERE id_orden = ?', [id]);
-
         res.status(200).json({ message: 'Orden de trabajo eliminada con éxito' });
     } catch (error) {
         res.status(500).json({ error: error.message });
